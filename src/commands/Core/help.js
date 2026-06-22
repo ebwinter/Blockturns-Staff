627e0132bfbb9153883a081020f58dfa370d76bd
ebwinter <robloxgamiing220@gmail.com>
2026-06-22 12:11:02 +1000

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -100,7 +100,7 @@ export async function createInitialHelpMenu(client) {
             },
             {
                 name: '\u200B',
-                value: `-# ${botName} is [open source](https://youtu.be/1jCZX8s3bJE?si=NPOYx-vxVE1I5vJK)`,
+                value: `-# ${botName} is [made by ebwinter for blockturns](https://discord.gg/yf7j5xxCwW)`,
                 inline: false,
             },
         ],
